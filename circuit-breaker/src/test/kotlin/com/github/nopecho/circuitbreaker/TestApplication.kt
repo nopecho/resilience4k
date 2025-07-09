d727f211abc90465f547b00da08e01989bcd1029
nopecho <ch.jooon@gmail.com>
2025-07-09 22:41:52 +0900

    chore: 테스트용 애플리케이션 설정 변경
    테스트를 위해 메인 클래스명을 변경하고 BootJar 생성을 비활성화.

--- circuit-breaker/src/test/kotlin/com/github/nopecho/circuitbreaker/TestApplication.kt
+++ circuit-breaker/src/test/kotlin/com/github/nopecho/circuitbreaker/TestApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CircuitBreakerApplication
+class TestApplication
 
 fun main(args: Array<String>) {
-    runApplication<CircuitBreakerApplication>(*args)
+    runApplication<TestApplication>(*args)
 }
